3e2b5fd0a8b426269acdf412a50e5dbe4435d46a
Adam Black <black@ohdsi.org>
2023-03-26 16:40:13 +0000

    version 0.6.3

--- R/Object.R
+++ R/Object.R
@@ -451,3 +451,25 @@ checkIfValid <- function(x) {
   if (!isValidAndromeda(x))
     abort("Andromeda object is no longer valid. Perhaps it was saved without maintainConnection = TRUE, or R has been restarted?")
 }
+
+#' Is the object an Andromeda table?
+#'
+#' @param tbl A reference to an Andromeda table
+#'
+#' @return TRUE or FALSE
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' andr <- andromeda(cars = cars)
+#' isAndromedaTable(andr$cars)
+#' close(andr)
+#' }
+isAndromedaTable <- function(tbl) {
+  return(
+    inherits(tbl, "FileSystemDataset") ||
+    inherits(tbl, "arrow_dplyr_query") ||
+    inherits(tbl, "tbl_Andromeda") ||
+    inherits(tbl, "tbl_SQLiteConnection")
+  )
+}
